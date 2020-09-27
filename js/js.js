ecc87deefd522d94135a27b10fbca72bce723219
warreir <warreir@gmail.com>
2020-09-27 09:51:35 -0300

    Correções

--- js/js.js
+++ js/js.js
@@ -1,37 +1,23 @@
 var objetos = { id:[], texto:[], qtde:[] }
 
+function mouse(){
+    alert("Vamos cadastrar agora, preencha os campos!");
+}
 function bemVindo(){
     alert("Bem vindo ao cadastro de produtos.");
     var produto = prompt("O que iremos registrar hoje?");
     document.getElementById("produtoTitulo").textContent = produto;
     document.getElementById("produtoLista").textContent = produto;
-    botaoAdicionar();
+    criaBotaoAdicionar();
 }
-function botaoAdicionar(){
-    var botaoAdicionar = document.createElement('button');
-    botaoAdicionar.setAttribute("onclick","adicionar()");
-    botaoAdicionar.setAttribute("type","button");
-    botaoAdicionar.textContent = "Adicionar";
-    document.getElementById("botoes").appendChild(botaoAdicionar);
-}
-function mouse(){
-    alert("Vamos cadastrar agora, preencha os campos!");
-}
-
-function adicionar(){
-    event.preventDefault();
-
-    // captura o que foi digitado nos inputs de tela
+function adicionarLinha(){
     var inputTexto = document.getElementById("texto");
     var inputQtde = document.getElementById("qtde");
-
-    //valida se ele digitou o nome do produto
     if(inputTexto.value == ""){
         alert("Preencha o nome");
     }else if(inputQtde.value == ""){
         alert("Preencha o qtde");
     }else{
-        //atribui os valores para o objeto que sal
         objetos.id.push(objetos.id.length+1);
         objetos.texto.push(inputTexto.value);
         objetos.qtde.push(inputQtde.value);
@@ -41,74 +27,95 @@ function adicionar(){
         document.getElementById("texto").value="";
         document.getElementById("qtde").value="";
     }
-   
 }
-
 function update(objetos){
-    document.getElementById("conteudo").innerHTML = "";
+
+    document.getElementById("tbodyTabela").innerHTML = "";
+    var tabela = document.getElementById("tbodyTabela");
+
      objetos.id.forEach(idSelect => {
-        //cria um td para tabela
-        var linha = document.createElement("tr");
+        var linha = tabela.insertRow();
 
-        // cria o td de id e coloca o valor na tabela
-        var colunaId = document.createElement("td");
-        colunaId.innerText = idSelect;
+        var cel1 = linha.insertCell(0);
+        var cel2 = linha.insertCell(1);
+        var cel3 = linha.insertCell(2);
+        var cel4 = linha.insertCell(3);
 
-        // cria o td de texto e coloca o nome na tabela
-        var colunaTexto = document.createElement("td");
-        colunaTexto.innerText = objetos.texto[idSelect-1]
+        cel1.innerText = idSelect;
+        cel2.innerText = objetos.texto[idSelect-1]
+        cel3.innerText = objetos.qtde[idSelect-1];
 
-        // cria o td de qtde e coloca o valor na tabela
-        var colunaQtde = document.createElement("td");
-        colunaQtde.innerText = objetos.qtde[idSelect-1];
+        var botaoAlterar = criaBotaoAlterar(idSelect-1)
+        cel4.appendChild(botaoAlterar);
+        
+        var botaoExcluir = criaBotaoExcluir(idSelect-1)
+        cel4.appendChild(botaoExcluir);
 
-        // cria o td para ação de alterar e excluir
-        var colunaAcao = document.createElement("td");
-        // cria o a para linkar
-        var linkAlterar = document.createElement('a');
-        // passa o onclick para excluir
-        linkAlterar.setAttribute("onclick","alterarProduto("+(idSelect-1)+")");
-        linkAlterar.innerText = "a";
-        linkAlterar.className = "alterar";
-        //coloca o a dentro do td
-        colunaAcao.appendChild(linkAlterar);
-
-        // cria o a para linkar
-        var linkExcluir = document.createElement('a');
-        // passa o onclick para excluir
-        linkExcluir.setAttribute("onclick","excluir("+(idSelect-1)+")");
-        linkExcluir.innerText = "x";
-        linkExcluir.className = "excluir";
-        //coloca o a dentro do td
-        colunaAcao.appendChild(linkExcluir);
-
-        //adiciona os td's ao tr
-        linha.appendChild(colunaId);
-        linha.appendChild(colunaTexto);
-        linha.appendChild(colunaQtde);
-        linha.appendChild(colunaAcao);
-
-        document.getElementById("conteudo").appendChild(linha);
+        linha.appendChild(cel1);
+        linha.appendChild(cel2);
+        linha.appendChild(cel3);
+        linha.appendChild(cel4);
      });
      
 }
 
-function alterarProduto(id){
+function criaBotaoAdicionar(){
+    var botao = document.createElement('button');
+    botao.setAttribute("onclick","adicionarLinha()");
+    botao.type = "button";
+    botao.className = "col-12 btn btn-primary btn-sm";
+    botao.textContent = "Adicionar";
+    document.getElementById("botoes").appendChild(botao);
+}
+
+function criaBotaoSalvar(id){
+    var botao = document.createElement('button');
+    botao.setAttribute("onclick","alterarLinha("+id+")");
+    botao.type = "button";
+    botao.className = "col-12 btn btn-secondary btn-sm";
+    botao.textContent = "Alterar";
+    document.getElementById("botoes").appendChild(botao);
+}
+
+function criaBotaoAlterar(id){
+    var botao = document.createElement('button');
+    var icon = criarIcone("fa fa-pencil-square-o");
+    botao.appendChild(icon);
+
+    botao.setAttribute("onclick","selecionaParaAlterar("+(id)+")");
+    botao.type = "button";
+    botao.className = "col-5 btn btn-warning btn-sm mr-1";
+    
+    return botao;
+}
+
+function criaBotaoExcluir(id){
+    var botao = document.createElement('button');
+    var icon = criarIcone("fa fa-trash-o");
+    botao.appendChild(icon);
+
+    botao.setAttribute("onclick","excluirLinha("+(id)+")");
+    botao.type = "button";
+    botao.className = "col-5 btn btn-danger btn-sm ml-1";
+    
+    return botao;
+}
+
+function criarIcone(classe){
+    var icon = document.createElement("i");
+    icon.setAttribute("aria-hidden","true");
+    icon.className = classe;
+    return icon;
+}
+
+function selecionaParaAlterar(id){
     document.getElementById("botoes").innerHTML = "";
-
-    var botaoAdicionar = document.createElement('button');
-    botaoAdicionar.setAttribute("onclick","alterarObjeto("+id+")");
-    botaoAdicionar.setAttribute("type","button");
-    botaoAdicionar.textContent = "Alterar";
-
-    document.getElementById("botoes").appendChild(botaoAdicionar);
-
+    criaBotaoSalvar(id);
     document.getElementById("texto").value = objetos.texto[id];
     document.getElementById("qtde").value = objetos.qtde[id];
 }
 
-function alterarObjeto(id){
-    
+function alterarLinha(id){
     objetos.texto[id] =  document.getElementById("texto").value;
     objetos.qtde[id] =  document.getElementById("qtde").value;
     
@@ -117,10 +124,10 @@ function alterarObjeto(id){
     document.getElementById("qtde").value = "";
 
     update(objetos);
-    botaoAdicionar();
+    criaBotaoAdicionar();
 }
 
-function excluir(id){
+function excluirLinha(id){
     delete objetos.id[id];
     delete objetos.texto[id];
     delete objetos.qtde[id];
